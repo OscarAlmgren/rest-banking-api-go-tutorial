c2c7860c62001ff2674de74afff017a09c4a2216
Oscar Almgren <oscar.almgren@gmail.com>
2023-01-09 16:04:51 +0100

    section 2 router basics but use Echo go webserver

--- main.go
+++ main.go
@@ -6,12 +6,31 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+}
+
 func main() {
-	echo := echo.New()
-	echo.GET("/", rootGetHandler)
-	echo.Logger.Fatal(echo.Start(":3000"))
+	e := echo.New()
+	e.GET("/", rootGetHandler)
+
+	e.GET("/customers", getAllCustomers)
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
 
 func rootGetHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, world!")
+}
+
+func getAllCustomers(c echo.Context) error {
+	customers := []Customer{
+		{"Oscar", "Nacka", "13131"},
+		{"Ida", "Kvarnholmen", "13131"},
+		{"Hugo", "Lilla Kvarnholmen", "12345"},
+	}
+
+	return c.JSON(http.StatusOK, customers)
 }
