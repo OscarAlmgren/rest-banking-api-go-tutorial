c13ebae6390380be86f53b7f9ddbf962dfe69540
Oscar Almgren <oscar.almgren@gmail.com>
2023-01-13 16:04:56 +0100

    CustomerRepository added

---
+++ domain/customer.go
@@ -0,0 +1,19 @@
+package domain
+
+// business object
+type Customer struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	City        string `json:"city"`
+	Zipcode     string `json:"zipcode"`
+	DateofBirth string
+	Status      string
+}
+
+// repository
+type CustomerRepository interface {
+	FindAll() ([]Customer, error)
+	// FindCustomer(id int) (Customer, error)
+	// UpdateCustomer(id int) (Customer, error)
+	// DeleteCustomer(id int) (Customer, error)
+}
