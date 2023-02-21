e7fb7b3fe153594f30555250bcfb3453ba3a8c68
Oscar Almgren <oscar.almgren@gmail.com>
2023-02-21 15:14:57 +0100

    Add zerologger to project

--- app/app.go
+++ app/app.go
@@ -1,13 +1,13 @@
 package app
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 	"github.com/oscaralmgren/rest-banking-api-go-tutorial/domain"
 	"github.com/oscaralmgren/rest-banking-api-go-tutorial/service"
+	"github.com/rs/zerolog/log"
 )
 
 func Start() {
@@ -23,6 +23,6 @@ func Start() {
 	e.GET("/customers/:id", ch.getCustomerById)
 
 	if err := e.Start(":3000"); err != http.ErrServerClosed {
-		log.Fatal(err)
+		log.Fatal().Str("Error", err.Error()).Msg("Server error on startup")
 	}
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,13 @@ require github.com/labstack/echo/v5 v5.0.0-20220717203827-74022662be4a
 require github.com/go-sql-driver/mysql v1.7.0
 
 require (
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/rs/zerolog v1.29.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
+
+require (
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
